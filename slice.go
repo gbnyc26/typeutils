dc99457f863cc93830d93dc0e511d2881d7651eb
gbnyc26 <gregbehrend26@gmail.com>
2020-07-13 14:17:37 -0400

    Added Byte Slice functions

--- slice.go
+++ slice.go
@@ -2,6 +2,13 @@ package typeutils
 
 import "strings"
 
+func EmptyByteSliceNil(b []byte) []byte {
+	if len(b) == 0 {
+		return nil
+	}
+	return b
+}
+
 func SplitEmptyStringSliceEmpty(s string, sep string) []string {
 	if s == "" {
 		return []string{}
