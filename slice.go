8909138d1a1ae63aebc2776715abeb7464186402
gbnyc26 <gregbehrend26@gmail.com>
2021-10-02 08:51:59 -0400

    Initial Commit

--- slice.go
+++ slice.go
@@ -2,6 +2,25 @@ package typeutils
 
 import "strings"
 
+func IndexStringSlice(slice []string) map[string]bool {
+	// if slice is nil return an empty index
+
+	r := make(map[string]bool)
+	for _, s := range slice {
+		r[s] = true
+	}
+
+	return r
+}
+
+func NilStringSliceEmpty(slice []string) []string {
+	if slice == nil {
+		return []string{}
+	}
+
+	return slice
+}
+
 func EmptyByteSliceNil(b []byte) []byte {
 	if len(b) == 0 {
 		return nil
